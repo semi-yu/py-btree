495b36dd54871ed17242e4dee73e985e3e115e7b
armedHeistKino <urban.manul.1919@gmail.com>
2025-03-29 18:59:41 +0900

    feat: 엔트리 클래스 메서드 추가

--- src/key_entry.py
+++ src/key_entry.py
@@ -1,7 +1,21 @@
 """
 
 """
+from typing import Any
 
 
 class KeyEntry:
-    ...
+    def __init__(self, key: int, value: Any = None):
+        self._key = key
+        self._value = value
+
+    def __repr__(self):
+        return f'({'🔑' if self.key and not self.value else ''}{self._key}:{self._value if self._value else ''})'
+    
+    @property
+    def key(self):
+        return self._key
+    
+    @property
+    def value(self):
+        return self._value

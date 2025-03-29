d9cf0e0c444cde2837891ce26d0b340ebddb1c79
armedHeistKino <urban.manul.1919@gmail.com>
2025-03-29 18:59:10 +0900

    feat: 검색 결과 클래스 매직 메서드 추가

--- src/entry_result.py
+++ src/entry_result.py
@@ -4,4 +4,17 @@
 
 
 class EntryResult:
-    ...
+    def __init__(self, found: bool = False, node = None, index: int = None):
+        self._found = found
+        self._node = node
+        self._index = index
+    
+    def __bool__(self):
+        return self._found
+
+    def __iter__(self):
+        yield self._node
+        yield self._index
+
+    def __repr__(self):
+        return f'({self._node}, {self._index})' if self._found else '(Not found)'

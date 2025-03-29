305b7b0473b34d751672cea687a9aa42a0b40ddc
armedHeistKino <urban.manul.1919@gmail.com>
2025-03-29 20:07:52 +0900

    feat: 노드 번호 기능 삭제

--- src/node.py
+++ src/node.py
@@ -6,16 +6,12 @@ from bisect import bisect_left, bisect_right
 from key_entry import KeyEntry
 from entry_result import EntryResult
 
-n_node = 0
-
 
 class Node:
     def __init__(self,
                  order: int,
                  is_root: bool = False, is_leaf: bool = True,
                  key: list[KeyEntry] = list(), children: list = list()):
-        global n_node
-
         self._order: int = order
 
         self.is_root: bool = is_root
@@ -23,9 +19,6 @@ class Node:
 
         self._keys: list[KeyEntry] = key
         self._children: list = children
-
-        self.n_node = n_node
-        n_node += 1
 
     def __len__(self):
         return len(self.keys)

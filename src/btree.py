c9c7498b94ae0db0134de40d5936596118563b06
armedHeistKino <urban.manul.1919@gmail.com>
2025-03-29 18:57:16 +0900

    feat: 검색 및 삽입 기능 추가

--- src/btree.py
+++ src/btree.py
@@ -1,6 +1,12 @@
 """
 
 """
+from bisect import bisect_left
+
+from traverse import traverse
+
+from key_entry import KeyEntry
+from entry_result import EntryResult
 from node import Node
 
 
@@ -9,6 +15,67 @@ class BTree:
                  order: int):
         self._order = order
 
-        self._root = Node(
+        self.root = Node(
             order = order,
             is_root = True, is_leaf = True)
+        
+    def __repr__(self):
+        return traverse(self.root)
+
+    def search(self, key: int, exact: bool = False):
+        parent = None
+        current = self.root
+
+        while not current.is_leaf:
+            index = bisect_left(current.keys, key, key = lambda entry: entry.key)
+
+            if index < len(current) and current.keys[index].key == key:
+                return EntryResult(True, current, index)
+
+            parent = current
+            current = current.children[index]
+
+        return parent, current.search(key, exact)
+    
+    def insert(self, entry: KeyEntry):
+        parent, result = self.search(entry.key)
+
+        target, index = result
+
+        target._keys.insert(index, entry)
+
+        if target.is_overflow():
+            self.split(parent, target)
+
+    def split(self, parent: Node, target: Node):
+        mid_point = len(target) // 2
+        mid_key = target.keys[mid_point]
+            
+        if target.is_root:
+            left_node = Node(order = self.order,
+                        is_root = False, is_leaf = target.is_leaf,
+                        key = target._keys[:mid_point], children = target._children[:mid_point+1])
+            right_node = Node(order = self.order, is_leaf = target.is_leaf,
+                         key = target._keys[mid_point+1:], children = target._children[mid_point+1:])
+
+            new_root = Node(order = self.order,
+                            is_root = True, is_leaf = False,
+                            key = [mid_key], children = [left_node, right_node])
+
+            self.root = new_root
+    
+    @property
+    def order(self):
+        return self._order
+
+if __name__ == '__main__':
+    tree = BTree(5)
+
+    tree.root._keys = [KeyEntry(2, 10), KeyEntry(3, 30), KeyEntry(17, 30), KeyEntry(22, 30), KeyEntry(31, 30),]
+
+    tree.insert(KeyEntry(7, 70))
+    
+    print(tree.search(7, exact = True))
+
+    print(tree)
+
--- src/node.py
+++ src/node.py
@@ -1,17 +1,61 @@
 """
 
 """
+from bisect import bisect_left, bisect_right
+
 from key_entry import KeyEntry
+from entry_result import EntryResult
+
+n_node = 0
+
 
 class Node:
     def __init__(self,
                  order: int,
                  is_root: bool = False, is_leaf: bool = True,
                  key: list[KeyEntry] = list(), children: list = list()):
+        global n_node
+
         self._order: int = order
 
-        self._is_root: bool = is_root
-        self._is_leaf: bool = is_leaf
-        
-        self._key: list[KeyEntry] = key
+        self.is_root: bool = is_root
+        self.is_leaf: bool = is_leaf
+
+        self._keys: list[KeyEntry] = key
         self._children: list = children
+
+        self.n_node = n_node
+        n_node += 1
+
+    def __len__(self):
+        return len(self.keys)
+    
+    def __repr__(self):
+        return f'#{self.n_node:03} ({self.keys}{'🫚 ' if self.is_root else ''}{'🌿' if self.is_leaf else ''})'
+
+    def search(self, key: int, exact: bool = False):
+        if exact:
+            index = bisect_left(self.keys, key, key = lambda entry: entry.key)
+
+            if index < len(self) \
+            and self.keys[index].key != key:
+                return EntryResult()
+        else:
+            index = bisect_right(self.keys, key, key = lambda entry: entry.key)
+
+        return EntryResult(True, self, index)
+
+    def is_overflow(self):
+        return len(self) >= self.order
+    
+    @property
+    def order(self):
+        return self._order
+
+    @property
+    def keys(self):
+        return self._keys
+    
+    @property
+    def children(self):
+        return self._children

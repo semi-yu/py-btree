c00360d78d217576c2b2aa2d4f505928da1eb2a6
armedHeistKino <urban.manul.1919@gmail.com>
2025-03-29 15:47:46 +0900

    feat: BTree 클래스 정의 및 생성자 추가

---
+++ src/btree.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+from node import Node
+
+
+class BTree:
+    def __init__(self,
+                 order: int):
+        self._order = order
+
+        self._root = Node(
+            order = order,
+            is_root = True, is_leaf = True)
